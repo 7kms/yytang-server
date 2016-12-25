067195a98cbb1df86e37ae2b5dd0225af4b2f1b2
baotang <tlyspa@qq.com>
2016-12-26 01:21:57 +0800

    loginout

--- routes/authority.js
+++ routes/authority.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 router.use(function(req,res,next){
+    console.log(req.session);
     if(!req.session.userID){
         res.status(401).json({code:401,msg:'未登录'})
     }else{
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,14 @@ router.use(author)
 router.get('/info', (req, res, next) =>{
     res.status(200).json({userInfo:req.session.userInfo})
 })
+router.post('/loginout', (req, res, next) => {
+    console.log(req);
+    req.session.destroy(err => {
+        if(!err) {
+            res.status(200).json({code:200,msg:'退出登录'})
+        }else {
+            res.status(400).json({code: 400,msg:'退出失败'})
+        }
+    })
+})
 module.exports = router
