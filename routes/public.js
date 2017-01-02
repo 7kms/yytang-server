1802296f5e2ff10889e1a4a6052625d932dd6070
baotang <tlyspa@qq.com>
2017-01-03 01:08:15 +0800

    server

--- routes/public.js
+++ routes/public.js
@@ -1,8 +1,4 @@
 var router = require('express').Router();
 var juejin = require('../juejin/index.js');
 router.use('/juejin',juejin)
-router.get('/test',(req, res, next)=>{
-    console.log(req)
-  res.status(200).json({msg:'da'})
-})
 module.exports = router
