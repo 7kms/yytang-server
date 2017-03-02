a04ea46522e0f54f0a995c71e351e07bf25f1aa9
唐亮 <tangliang@mofanghr.com>
2017-03-02 18:12:12 +0800

    'ssr'

--- config/setting.js
+++ config/setting.js
@@ -2,7 +2,7 @@ var path = require('path')
 module.exports = {
     port: 9000,
     cross: true,
-    whiteList: ['http://localhost:3456','htts://localhost'],
+    whiteList: ['http://localhost:3456','htts://localhost','http://localhost:8080'],
     ssl:{
         active: false,
         key: path.resolve(__dirname,'cert/yytang-key.pem'),
