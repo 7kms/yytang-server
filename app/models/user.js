dbc0ca2c05032bc386c4819b70c41a8ba8394ac3
baotang <tlyspa@qq.com>
2017-05-15 00:59:09 +0800

    eslint

--- app/models/user.js
+++ app/models/user.js
@@ -14,17 +14,17 @@ const Schema = mongoose.Schema;
  */
 
 const UserSchema = new Schema({
-  mobile: { type: String, default: '' },
-  name: { type: String, default: '' , trim: true},
-  hashed_password: { type: String, default: '' },
-  email: { type: String, required: true , trim: true, unique: true},
-  createTime: { type: Date, default: Date.now },
-  updateTime: {
-    type: Date,
-    default: ()=>{
-      return this.createTime
+    mobile: { type: String, default: '' },
+    name: { type: String, default: '', trim: true },
+    hashed_password: { type: String, default: '' },
+    email: { type: String, required: true, trim: true, unique: true },
+    createTime: { type: Date, default: Date.now },
+    updateTime: {
+        type: Date,
+        default: () => {
+            return this.createTime
+        }
     }
-  }
 });
 UserSchema.plugin(uniqueValidator);
 
@@ -37,23 +37,23 @@ UserSchema.plugin(uniqueValidator);
 // },'Invalid mobile')
 
 UserSchema.path('email').validate(value => {
-  return  /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/i.test(value);
-},'Invalid mobile')
+    return /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/i.test(value);
+}, 'Invalid email')
 
 /**
  * Virtuals
  */
 
 UserSchema
-  .virtual('password')
-  .set(function (password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
-  })
-  .get(function () {
-    return this._password;
-  });
+    .virtual('password')
+    .set(function(password) {
+        this._password = password;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
 
 
 /**
@@ -62,62 +62,62 @@ UserSchema
 
 UserSchema.methods = {
 
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
+    /**
+     * Authenticate - check if the passwords are the same
+     *
+     * @param {String} plainText
+     * @return {Boolean}
+     * @api public
+     */
 
-  authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password;
-  },
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
 
 
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
+    /**
+     * Make salt
+     *
+     * @return {String}
+     * @api public
+     */
 
-  makeSalt: function () {
-    return Math.round((new Date().valueOf() * Math.random())) + '';
-  },
-  /**
-   * Encrypt password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
+    makeSalt: function() {
+        return Math.round((new Date().valueOf() * Math.random())) + '';
+    },
+    /**
+     * Encrypt password
+     *
+     * @param {String} password
+     * @return {String}
+     * @api public
+     */
 
-  encryptPassword: function (password) {
-    if (!password) return '';
-    try {
-      return crypto
-        .createHmac('sha1', this.salt)
-        .update(password)
-        .digest('hex');
-    } catch (err) {
-      return '';
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
     }
-  }
 
 };
 
 UserSchema.statics = {
-  load(conditionObj){
-    return new Promise((resolve,reject) => {
-       this.findOne(conditionObj).exec(function(err, user) {
-          if(err){
-            reject(err);
-          }
-          resolve(user);
-      });
-    })
-  }
+    load(conditionObj) {
+        return new Promise((resolve, reject) => {
+            this.findOne(conditionObj).exec(function(err, user) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(user);
+            });
+        })
+    }
 }
 
 /**
